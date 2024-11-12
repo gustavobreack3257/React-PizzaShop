132ecc3cc779331f7e79f79ee81211f25bfe5f21
Gustavo Costa Souza <drt65211@MAC432701-M.local>
2024-11-12 14:32:15 -0300

    feat: Connection to API - Profile in header

---
+++ src/api/get-profile.ts
@@ -0,0 +1,15 @@
+import { api } from '@/lib/axios'
+
+interface GetProfileResponse {
+  id: string
+  name: string
+  email: string
+  phone: string | null
+  role: 'manager' | 'customer'
+}
+
+export async function getProfile() {
+  const response = await api.get<GetProfileResponse>('/me')
+
+  return response.data
+}
--- src/api/sign-in.ts
+++ src/api/sign-in.ts
@@ -3,6 +3,7 @@ import { api } from '@/lib/axios'
 export interface SignInBody {
   email: string
 }
+
 export async function signIn({ email }: SignInBody) {
   await api.post('/authenticate', { email })
 }
