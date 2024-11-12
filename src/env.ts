da1989314870aee1fb306444fbb2bc89f4068d41
Gustavo Costa Souza <drt65211@MAC432701-M.local>
2024-11-12 15:51:43 -0300

    feat: Conexão com API - Loading state no perfil

--- src/env.ts
+++ src/env.ts
@@ -2,6 +2,10 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   VITE_API_URL: z.string(),
+  VITE_ENABLE_API_DELAY: z
+    .string()
+    .default('true')
+    .transform((value) => value === 'true'),
 })
 
 export const env = envSchema.parse(import.meta.env)
