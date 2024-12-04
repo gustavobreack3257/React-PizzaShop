759239532d0b6794c8303da8014624bcbda58668
Gustavo Costa Souza <drt65211@MAC432701-M.local>
2024-12-04 09:10:12 -0300

    feat: Loading & Empty states - Loading of orders

--- src/pages/app/Dashboard/popular-products-chart.tsx
+++ src/pages/app/Dashboard/popular-products-chart.tsx
@@ -98,7 +98,7 @@ export function PopularProductsChart() {
             </PieChart>
           </ResponsiveContainer>
         ) : (
-          <div className="flex h-[240px] w-full items-center justify-center align-baseline">
+          <div className="flex h-[240px] w-full items-center justify-center">
             <Loader2 className="h-8 w-8 animate-spin text-muted-foreground" />
           </div>
         )}
--- src/pages/app/orders.tsx
+++ src/pages/app/orders.tsx
@@ -5,7 +5,6 @@ import { z } from 'zod'
 
 import { getOrders } from '@/api/get-orders'
 import { Pagination } from '@/components/pagination'
-import { Input } from '@/components/ui/input'
 import {
   Table,
   TableBody,
@@ -16,6 +15,7 @@ import {
 
 import { OrderTableFilters } from './order-table-filters'
 import { OrderTableRow } from './order-table-row'
+import { OrderTableSkeleton } from './order-table-skeleton'
 
 export function Orders() {
   const [searchParams, setSearchParams] = useSearchParams()
@@ -29,7 +29,7 @@ export function Orders() {
     .transform((page) => page - 1) // índice da pagina
     .parse(searchParams.get('page') ?? '1') // índice apresentado ao usuário
 
-  const { data: result } = useQuery({
+  const { data: result, isLoading: isLoadingOrders } = useQuery({
     queryKey: ['orders', pageIndex, orderId, customerName, status],
     queryFn: () =>
       getOrders({
@@ -50,39 +50,33 @@ export function Orders() {
   return (
     <>
       <Helmet title="Pedidos" />
-
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
-      </div>
-      <div className="space-y-2.5">
-        <form className="flex items-center gap-2">
-          <span className="text-sm font-semibold">Filtros</span>
-          <Input placeholder="Nome do cliente" className="h-8 w-[320px]" />
-        </form>
-
-        <OrderTableFilters />
-
-        <div>
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="w-[64px]"></TableHead>
-                <TableHead className="w-[140px]">Identificador</TableHead>
-                <TableHead className="w-[180px]">Realizado há</TableHead>
-                <TableHead className="w-[140px]">Status</TableHead>
-                <TableHead>Cliente</TableHead>
-                <TableHead className="w-[140px]">Total do pedido</TableHead>
-                <TableHead className="w-[164px]"></TableHead>
-                <TableHead className="w-[132px]"></TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {result &&
-                result.orders.map((order) => {
-                  return <OrderTableRow key={order.orderId} order={order} />
-                })}
-            </TableBody>
-          </Table>
+        <div className="space-y-2.5">
+          <OrderTableFilters />
+          <div className="rounded-md border">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[64px]"></TableHead>
+                  <TableHead className="w-[140px]">Identificador</TableHead>
+                  <TableHead className="w-[180px]">Realizado há</TableHead>
+                  <TableHead className="w-[140px]">Status</TableHead>
+                  <TableHead>Cliente</TableHead>
+                  <TableHead className="w-[140px]">Total do pedido</TableHead>
+                  <TableHead className="w-[164px]"></TableHead>
+                  <TableHead className="w-[132px]"></TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {result &&
+                  result.orders.map((order) => {
+                    return <OrderTableRow key={order.orderId} order={order} />
+                  })}
+              </TableBody>
+            </Table>
+          </div>
+          {isLoadingOrders && <OrderTableSkeleton />}
           {result && (
             <Pagination
               onPageChange={handlePaginate}
