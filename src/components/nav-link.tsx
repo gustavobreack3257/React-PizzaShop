ecd7f05e23beab005889528ef8329c29b10e340f
Gustavo Costa Souza <drt65211@MAC432701-M.local>
2024-12-10 15:37:35 -0300

    feat: Testing and mocks - unit test : NavLink

--- src/components/nav-link.tsx
+++ src/components/nav-link.tsx
@@ -1,9 +1,11 @@
+import React from 'react'
 import { Link, LinkProps, useLocation } from 'react-router-dom'
 
 export type NavLinkProps = LinkProps
 
 export function NavLink(props: NavLinkProps) {
   const { pathname } = useLocation()
+
   return (
     <Link
       data-current={pathname === props.to}
