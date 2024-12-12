ffb7db373ddf4dbd4c3b609340febf71835e0eab
Gustavo Costa Souza <drt65211@MAC432701-M.local>
2024-12-12 15:40:07 -0300

    feat: Testing and mocks - profile mock and update

--- src/api/get-profile.ts
+++ src/api/get-profile.ts
@@ -1,11 +1,13 @@
 import { api } from '@/lib/axios'
 
-interface GetProfileResponse {
+export interface GetProfileResponse {
   id: string
   name: string
   email: string
   phone: string | null
   role: 'manager' | 'customer'
+  createdAt?: Date | null
+  updatedAt?: Date | null
 }
 
 export async function getProfile() {
